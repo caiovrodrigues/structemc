c71be8703cf4f98b68860c2b3556b92643a26424
caiovrodrigues <caiobiscas@gmail.com>
2023-08-24 23:33:03 -0300

    First commit

---
+++ Questao 1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*Crie uma struct chamada "Pessoa" que contenha os seguintes campos: nome, idade e altura. Em seguida, 
+escreva um programa que declare uma variável do tipo Pessoa, preencha seus campos e imprima os valores.*/
+
+typedef struct{
+	char nome[20];
+	int idade;
+	float altura;
+}Pessoa;
+
+int main(){
+	questao1();
+}
+
+void questao1(){
+	Pessoa caio;
+	char nome[20] = "caio";
+	strcpy(caio.nome, nome);
+	caio.idade = 22;
+	caio.altura = 1.50;
+	
+	printf("%s tem %d anos e %.2f de altura", caio.nome, caio.idade, caio.altura);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
