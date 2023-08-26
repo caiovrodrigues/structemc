1df7accc310c1173e61634715a44188c4bed77d0
caiovrodrigues <caiobiscas@gmail.com>
2023-08-25 22:20:02 -0300

    Second commit

--- Questao 2.c
+++ Questao 2.c
@@ -12,7 +12,7 @@ void imprimir(Tipo V){
 	printf("Letra: %s, Inteiro: %d, Float: %.1f\n\n", V.letra, V.inteiro, V.flutuante);
 }
 
-Tipo setPonto(char letra[1], int inteiro, float flutuante){
+Tipo setTipo(char letra[1], int inteiro, float flutuante){
 	Tipo V;
 	strcpy(V.letra, letra);
 	V.inteiro = inteiro;
@@ -22,22 +22,22 @@ Tipo setPonto(char letra[1], int inteiro, float flutuante){
 
 int main(){
 	Tipo Valor1;
-	Valor1 = setPonto("a", 1, 1.1);
+	Valor1 = setTipo("a", 1, 1.1);
 	imprimir(Valor1);
 	
 	Tipo Valor2;
-	Valor2 = setPonto("b", 2, 2.1);
+	Valor2 = setTipo("b", 2, 2.1);
 	imprimir(Valor2);
 	
 	Tipo Valor3;
-	Valor3 = setPonto("c", 3, 3.1);
+	Valor3 = setTipo("c", 3, 3.1);
 	imprimir(Valor3);
 	
 	Tipo Valor4;
-	Valor4 = setPonto("d", 4, 4.1);
+	Valor4 = setTipo("d", 4, 4.1);
 	imprimir(Valor4);
 	
 	Tipo Valor5;
-	Valor5 = setPonto("e", 5, 5.1);
+	Valor5 = setTipo("e", 5, 5.1);
 	imprimir(Valor5);
 }
